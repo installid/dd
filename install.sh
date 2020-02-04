6d5b2168f18fd26439b6925eba95e57e47eeef60
heathdutton <hdutton@thedmsgrp.com>
2020-02-04 11:08:42 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -82,9 +82,15 @@ if [ ! -z $( which ${DD_PLATFORM} ) ]
 then
   URL=$( curl -s "https://api.github.com/repos/DataDog/dd-trace-${DD_PLATFORM}/releases/latest" | grep -E "http.*datadog-${DD_PLATFORM}-tracer-.*\.x86_64\.rpm" | cut -d : -f 2,3 | cut -d '"' -f 2 )
   echo "Downloading ${URL}"
-  curl -s -L -o datadog-${DD_PLATFORM}-tracer.rpm "${URL}"
-  sudo rpm -ivh datadog-${DD_PLATFORM}-tracer.rpm
-  rm -f datadog-${DD_PLATFORM}-tracer.rpm
+  if [ -f datadog-${DD_PLATFORM}-tracer.rpm ]
+  then
+    curl -s -L -o datadog-${DD_PLATFORM}-tracer.rpm "${URL}"
+    sudo rpm -ivh datadog-${DD_PLATFORM}-tracer.rpm
+    rm -f datadog-${DD_PLATFORM}-tracer.rpm
+  else
+    echo "Could not download APM agent for this platform."
+    exit 1
+  fi
 fi
 
 EXPORTS="export DD_TRACE_CLI_ENABLED=true
