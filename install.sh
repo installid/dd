a27cc6a299ffc710f748231e1923128412ad7ab9
heathdutton <hdutton@thedmsgrp.com>
2020-02-04 10:47:34 -0500

    .

--- install.sh
+++ install.sh
@@ -51,11 +51,6 @@ then
 	echo "grep is required."
 	exit 1
 fi
-if [ -z $( which php ) ]
-then
-	echo "php is required."
-	exit 1
-fi
 
 if [ -f /opt/elasticbeanstalk/support/envvars ]
 then
@@ -64,12 +59,14 @@ fi
 
 cd /tmp
 
-URL=$( curl -s https://api.github.com/repos/DataDog/dd-trace-php/releases/latest | grep -E 'http.*datadog-php-tracer-.*\.x86_64\.rpm' | cut -d : -f 2,3 | cut -d '"' -f 2 )
-echo "Downloading ${URL}"
-curl -s -L -o datadog-php-tracer.rpm "${URL}"
-
-sudo rpm -ivh datadog-php-tracer.rpm
-rm -f datadog-php-tracer.rpm
+if [ !-z $( which php ) ]
+then
+  URL=$( curl -s https://api.github.com/repos/DataDog/dd-trace-php/releases/latest | grep -E 'http.*datadog-php-tracer-.*\.x86_64\.rpm' | cut -d : -f 2,3 | cut -d '"' -f 2 )
+  echo "Downloading ${URL}"
+  curl -s -L -o datadog-php-tracer.rpm "${URL}"
+  sudo rpm -ivh datadog-php-tracer.rpm
+  rm -f datadog-php-tracer.rpm
+fi
 
 EXPORTS="export DD_TRACE_CLI_ENABLED=true
 export DD_TRACE_ANALYTICS_ENABLED=true"
@@ -80,19 +77,28 @@ echo "External IP detected: ${IP}"
 DNS=$( curl -s https://api.hackertarget.com/reverseiplookup/?q=$IP )
 echo "DNS detected: ${DNS}"
 
-if [ -z "${DD_SERVICE_NAME}" ]
+if [ ! -z "${DD_API_KEY}" ]
 then
-	DD_SERVICE_NAME=$APP_NAME
+	bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
 fi
 
-read -p "What is the shortest domain of this service this machine is running (eg: servicename.com)? " servicename
+if [ -z "${DD_SERVICE_NAME}" ]
+then
+	DD_SERVICE_NAME="${APP_NAME}"
+fi
+
+if [ -z "${DD_SERVICE_NAME}" ]
+then
+  read -p "What is the shortest domain of this service this machine is running (eg: servicename.com)? " DD_SERVICE_NAME
+fi
+
 EXPORTS="${EXPORTS}
-export DD_SERVICE_NAME=${servicename}"
+export DD_SERVICE_NAME=${DD_SERVICE_NAME}"
 $EXPORTS
-echo -e $EXPORTS
+# echo -e $EXPORTS
 grep -qxF "$EXPORTS" /etc/environment || sudo echo -e "${EXPORTS}" >> /etc/environment
-echo "Contents of /etc/environment:"
-cat /etc/environment
+# echo "Contents of /etc/environment:"
+# cat /etc/environment
 
 # Newrelic would compete, kill it.
 rm -f /etc/php.d/newrelic.ini
@@ -100,17 +106,23 @@ rm -f /etc/php.d/newrelic.ini
 # Allow env variables to go into PHP-FPM
 # sudo sed -i 's/;clear_env = no/clear_env = no/g' /etc/php-fpm.d/www.conf
 
-# Shove the variables into FPM also.
-EXPORTSFPM="env[DD_TRACE_CLI_ENABLED] = true
+# Shove the variables into FPM.
+if [ if /etc/php-fpm.d/www.conf ]
+then
+  EXPORTSFPM="env[DD_TRACE_CLI_ENABLED] = true
 env[DD_TRACE_ANALYTICS_ENABLED] = true
-env[DD_SERVICE_NAME] = ${servicename}"
-grep -qxF "EXPORTSFPM" /etc/php-fpm.d/www.conf || sudo echo -e "${EXPORTSFPM}" >> /etc/php-fpm.d/www.conf
+env[DD_SERVICE_NAME] = ${DD_SERVICE_NAME}"
+  grep -qxF "EXPORTSFPM" /etc/php-fpm.d/www.conf || sudo echo -e "${EXPORTSFPM}" >> /etc/php-fpm.d/www.conf
+fi
 
 # Enable APM
 sudo sed -i 's/# apm_enabled: false/apm_enabled: true/g' /etc/dd-agent/datadog.conf
 
 # Upgrade DD agent if needed.
-DD_UPGRADE=true bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+if [ -z "${DD_API_KEY}" ]
+then
+  DD_UPGRADE=true bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+fi
 
 # Restart services.
 sudo service datadog-agent restart
